1a16a120189af267c4762f0b17696bb17c68d662
afzalimamias <afzalimamias@gmail.com>
2023-11-05 16:39:30 +0530

    change frontend url

--- backend/src/socket/socket.js
+++ backend/src/socket/socket.js
@@ -8,7 +8,7 @@ const socketUserMapping = {};
 export const initSocket = (server) => {
     io = new Server(server, {
         cors: {
-            origin: "http://localhost:3000",
+            origin: process.env.FRONTEND_URL,
             methods: ["GET", "POST"],
         },
     });
